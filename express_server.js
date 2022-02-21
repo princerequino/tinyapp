46c52beaf9cf307832903fc585d4bf53936735d1
princerequino <requino.prince@gmail.com>
2022-02-21 17:36:34 -0500

    added string generator + body parser

--- express_server.js
+++ express_server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set("view engine", "ejs");
+
+const generateRandomString = function(length = 6) {
+  return Math.random().toString(20).substr(2, length);
+};
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -34,7 +43,22 @@ app.get("/urls", (req, res) => {
 // req.params.shortURL ---> points to the KEYS in urlDatabase
 // urlDatabase[req.params.shortURL] ---> points to the Value of my Keys (longURLS)
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
+
